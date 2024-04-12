646cd4d433bbd3dc0daaa06b915342c9c0df2743
Risshi Kudeshia <risshi0706@gmail.com>
2024-04-13 04:27:37 +0530

    Time: 10 ms (83.95%) | Memory: 47.1 MB (5.52%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -3,20 +3,6 @@ class Solution {
         Stack<Integer> s = new Stack<>();
         int ans[] = new int[nums.length];
         Arrays.fill(ans,-1);
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i = 0;i<nums.length;i++){
-            hm.put(nums[i],i);
-        }
-        // for(int i = 0;i<nums.length;i++){
-        //     int current = nums[i];
-        //     while(!st.isEmpty() && current>st.peek()){
-        //         int popped = st.pop();
-        //         int index = hm.get(popped);
-        //         ans[index] = current;
-                
-        //     }
-        //     st.push(current);
-        // }
         for(int i=0; i<nums.length; i++){
             while(!s.isEmpty() && (nums[s.peek()]<nums[i])){
                 ans[s.peek()]=nums[i];
@@ -24,22 +10,12 @@ class Solution {
             }
             s.push(i);
         }
-        System.out.println(Arrays.toString(ans));
-        System.out.println(s.toString());
         for(int i=0; i<nums.length; i++){
             while(!s.isEmpty() && (nums[s.peek()]<nums[i])){
                 ans[s.peek()]=nums[i];
                 s.pop();
             }
-            // s.push(i);
         }
-        // for(int i = 0;i<hm.get(st.peek());i++){
-        //     int current = nums[i];
-        //     while(!st.isEmpty() && current>st.peek()){
-        //         ans[hm.get(st.peek())] = current;
-        //         st.pop();
-        //     }
-        // }
         return ans;
         
     }
