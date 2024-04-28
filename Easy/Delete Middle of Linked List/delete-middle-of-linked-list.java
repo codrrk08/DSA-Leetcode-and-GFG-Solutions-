3d717a93dda800b9368144f9b8d0b34605cf5e8a
Risshi Kudeshia <risshi0706@gmail.com>
2024-04-28 14:33:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete Middle of Linked List/delete-middle-of-linked-list.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            head = g.deleteMid(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+
+// } Driver Code Ends
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    Node deleteMid(Node head) {
+        
+        Node dummy = head;
+        Node p = dummy;
+        int count = 0;
+        while(dummy!=null){
+            count++;
+            dummy = dummy.next;
+        }
+        if(count==1||count==0)
+        return null;
+        int mid = count/2;
+        int index = 0;
+        Node ans = p;
+        while(true){
+            if(mid==index+1){
+                p.next = p.next.next;
+                break;
+            }
+            p = p.next;
+            index++;
+        }
+        return ans;
+    }
+}
