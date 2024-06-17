075e9e57154a033f8d41da8dc61912bc3a28ac67
Risshi Kudeshia <risshi0706@gmail.com>
2024-06-17 14:06:22 +0530

    Time: 4 ms (56.86%) | Memory: 39.8 MB (94.97%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,24 +1,32 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        int half =(int) Math.sqrt(c);
-        int arr[] = new int[half+1];
-        for(int i = 0;i<arr.length;i++){
-            arr[i] = i;
-        }
+        // int half =(int) Math.sqrt(c);
+        // int arr[] = new int[half+1];
         // for(int i = 0;i<arr.length;i++){
-        //     if(arr[i]*arr[i]+arr[i+1]*arr[i+1]==c)
+        //     arr[i] = i;
         // }
-        // System.out.println(Arrays.toString(arr));
-        HashMap<Integer,Integer> hm =new HashMap<>();
-        for(int i = 0;i<arr.length;i++){
-            hm.put(arr[i]*arr[i],arr[i]);
-        }
-        boolean result = false;
-        for(int i = 0;i<arr.length;i++){
-            if(hm.containsKey(c-(arr[i]*arr[i])))
+        // HashMap<Integer,Integer> hm =new HashMap<>();
+        // for(int i = 0;i<arr.length;i++){
+        //     hm.put(arr[i]*arr[i],arr[i]);
+        // }
+        // boolean result = false;
+        // for(int i = 0;i<arr.length;i++){
+        //     if(hm.containsKey(c-(arr[i]*arr[i])))
+        //     return true;
+        // }
+        // return result;
+        long half =(long) Math.sqrt(c);
+        long i = 0;
+        long j = half;
+        while(i<=j){
+            if(i*i+j*j==c)
             return true;
+            else if(i*i+j*j>c)
+            j--;
+            else
+            i++;
         }
-        return result;
+        return false;
         
     }
 }
