2d8252dee9c73372f3ab43c8229297e69e5df9cb
Risshi Kudeshia <risshi0706@gmail.com>
2024-07-25 15:32:06 +0530

    Time: 18 ms (86.59%) | Memory: 55.9 MB (57.59%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    // public void mergeSort(int nums[],int start,int end){
+    //     int mid = (start+end)/2;
+
+    // }
+    public int[] sortArray(int[] nums) {
+        // mergeSort(int nums,int 0,int nums.length-1);
+        Arrays.sort(nums);
+        return nums;
+    }
+}
