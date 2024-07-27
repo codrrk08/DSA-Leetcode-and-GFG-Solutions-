0fb995c3c899cf4b56f143702bb5927cf80ce348
Risshi Kudeshia <risshi0706@gmail.com>
2024-07-27 19:50:28 +0530

    Time: 23 ms (41.03%) | Memory: 50 MB (90.67%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,11 +1,11 @@
 class Solution {
     public int helper(String s1,String s2,int ind1,int ind2,int dp[][]){
-        if(ind1<0 || ind2<0)
+        if(ind1==0 || ind2==0)
         return 0;
         
         if(dp[ind1][ind2]!=-1)
         return dp[ind1][ind2];
-        if(s1.charAt(ind1)==s2.charAt(ind2)){
+        if(s1.charAt(ind1-1)==s2.charAt(ind2-1)){
             return 1 + helper(s1,s2,ind1-1,ind2-1,dp);
         }
         return dp[ind1][ind2] = Math.max(helper(s1,s2,ind1-1,ind2,dp),helper(s1,s2,ind1,ind2-1,dp));
@@ -17,6 +17,6 @@ class Solution {
         for(int row[]:dp){
             Arrays.fill(row,-1);
         }
-        return helper(text1,text2,m-1,n-1,dp);
+        return helper(text1,text2,m,n,dp);
     }
 }
