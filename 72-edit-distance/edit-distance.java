5826b3a377cb1c5187bb4e07ab5037f3e1007c37
Risshi Kudeshia <risshi0706@gmail.com>
2024-07-29 19:30:01 +0530

    Time: 5 ms (65.73%) | Memory: 45.1 MB (26.60%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -16,11 +16,27 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
         int dp[][] = new int[n+1][m+1];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        // for(int row[]:dp){
+        //     Arrays.fill(row,-1);
+        // }
+
+        //for i==0 all values of j will be j
+        for(int j = 0;j<=m;j++){
+            dp[0][j] = j;
+        }
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = i;
         }
 
-        return helper(word1,word2,n,m,dp);
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(word1.charAt(i-1)!=word2.charAt(j-1))
+                dp[i][j] = 1 + Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+                else
+                dp[i][j] = dp[i-1][j-1];
+            }
+        }
+        return dp[n][m];
 
     }
 }
