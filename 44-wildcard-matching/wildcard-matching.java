0a13e63b8f6e5ce80933f87585844de5dd965323
Risshi Kudeshia <risshi0706@gmail.com>
2024-07-30 00:22:32 +0530

    Time: 15 ms (83.62%) | Memory: 48.1 MB (34.92%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean helper(String s,String p,int i,int j,int dp[][]){
+        if(i<0 && j<0)
+        return true;
+        if(j<0 && i>=0)
+        return false;
+        if(j>=0 && i<0){
+            for(int x = 0;x<=j;x++){
+                if(p.charAt(x)!='*')
+                return false;
+            }
+            return true;
+        }
+        if(dp[i][j]!=-1)
+        return (dp[i][j]==0)?false:true;
+        boolean ans = false;
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+            ans = helper(s,p,i-1,j-1,dp);
+            dp[i][j] = (ans==true)?1:0;
+            return ans;
+        }
+        else if(p.charAt(j)=='*'){
+            ans = helper(s,p,i,j-1,dp) || helper(s,p,i-1,j,dp) ; 
+            dp[i][j] = (ans==true)?1:0;
+        }
+        else{
+            dp[i][j] = 0;
+            return false;
+        }
+        return ans;
+    }
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
+        }
+        return helper(s,p,n-1,m-1,dp);
+        // return false;
+    }
+}
