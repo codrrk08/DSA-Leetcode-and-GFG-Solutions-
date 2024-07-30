f9e7d0c666e9171512eb38b9661eaf4bc847e09e
Risshi Kudeshia <risshi0706@gmail.com>
2024-07-30 20:05:27 +0530

    Time: 4 ms (6.65%) | Memory: 45.7 MB (38.24%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -15,11 +15,31 @@ class Solution {
         return dp[index][canBuy] = profit;
     }
     public int maxProfit(int[] prices) {
+        // int n = prices.length;
+        // int dp[][] = new int[n+1][2];
+        // for(int row[]:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        // return helper(prices,0,1,dp);
         int n = prices.length;
         int dp[][] = new int[n+1][2];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+
+        for(int i = n-1;i>=0;i--){
+            for(int j = 0;j<=1;j++){
+                int profit = 0;
+                if(j==1){
+                    profit = Math.max(-prices[i] + dp[i+1][0],dp[i+1][1]);
+                }
+                else
+                profit = Math.max(prices[i] + dp[i+1][1],dp[i+1][0]);
+                dp[i][j] = profit;
+            }
         }
-        return helper(prices,0,1,dp);
+        return dp[0][1];
+
+
     }
 }
