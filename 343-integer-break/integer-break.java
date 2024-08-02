6132ed175f29c47cdb585e290926a4987a650dda
Risshi Kudeshia <risshi0706@gmail.com>
2024-08-02 22:48:35 +0530

    Time: 54 ms (5.03%) | Memory: 40.3 MB (44.23%) - LeetSync

---
+++ 343-integer-break/integer-break.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int helper(int arr[],int sum,int index){
+        if(sum==0)
+        return 1;
+        if(index<=0)
+        return 1;
+
+        int notPick = helper(arr,sum,index-1);
+        int pick = 1;
+        if(arr[index-1]<=sum){
+            pick = arr[index-1]*helper(arr,sum-arr[index-1],index);
+        }
+        // else
+        // notPick = arr[index]*helper(arr,sum-arr[index],index-1);
+
+        return Math.max(pick,notPick);
+    }
+    public int integerBreak(int n) {
+        if(n==1)
+        return 1;
+        int arr[] = new int[n-1];
+        for(int i = 0;i<n-1;i++){
+            arr[i] = i+1;
+        }
+        return helper(arr,n,n-1);
+    }
+}
