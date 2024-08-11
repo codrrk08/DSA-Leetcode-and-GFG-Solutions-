4c8278597c6727f2ba7f9c9afdfdd87b60e4256a
Risshi Kudeshia <risshi0706@gmail.com>
2024-08-11 18:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (5.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -15,10 +15,8 @@ class Solution {
     //     return dp[start] = count;
     // }
     public int helper(int n,int dp[]){
-        if(n<0)
-        return 0;
-        if(n==0)
-        return 1;
+        if(n<=2)
+        return n;
         
         if(dp[n]!=-1)
         return dp[n];
@@ -43,6 +41,11 @@ class Solution {
         // return dp[n];
         int dp[]  =new int[n+1];
         Arrays.fill(dp,-1);
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // for(int i = 2;i<n;i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
         return helper(n,dp);
     }
 }
