9bf929fdb12cbf6234aabdbcdb4bd66a6f9b2bf0
Risshi Kudeshia <risshi0706@gmail.com>
2024-08-11 23:24:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Geek Jump/geek-jump.java
+++ Difficulty: Easy/Geek Jump/geek-jump.java
@@ -31,26 +31,43 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    public int helper(int arr[],int n,int start,int dp[]){
-        if(start==n-1){
-            return 0;
-        }
-        // if(start>n-1)
-        // return 0;
-        // curr = start;
-        if(dp[start]!=-1)
-        return dp[start];
-        int leftDiff = helper(arr,n,start+1,dp) + Math.abs(arr[start]-arr[start+1]);
-        int rightDiff=Integer.MAX_VALUE;
-        if(start<n-2)
-        rightDiff = helper(arr,n,start+2,dp) + Math.abs(arr[start]-arr[start+2]);
+    // public int helper(int arr[],int n,int start,int dp[]){
+    //     if(start==n-1){
+    //         return 0;
+    //     }
+    //     // if(start>n-1)
+    //     // return 0;
+    //     // curr = start;
+    //     if(dp[start]!=-1)
+    //     return dp[start];
+    //     int leftDiff = helper(arr,n,start+1,dp) + Math.abs(arr[start]-arr[start+1]);
+    //     int rightDiff=Integer.MAX_VALUE;
+    //     if(start<n-2)
+    //     rightDiff = helper(arr,n,start+2,dp) + Math.abs(arr[start]-arr[start+2]);
         
         
-        return dp[start] = Math.min(leftDiff,rightDiff);
-    }
+    //     return dp[start] = Math.min(leftDiff,rightDiff);
+    // }
+    public int helper(int n,int arr[],int dp[]){
+    // 	if(n<0)
+    // 	return 0;
+    	if(n==0)
+    	return 0;
+    
+        if(dp[n]!=-1)
+        return dp[n];
+    	int twice = Integer.MAX_VALUE;
+    	int once = helper(n-1,arr,dp) + Math.abs(arr[n]-arr[n-1]);
+    	if(n>=2)
+    	twice = helper(n-2,arr,dp) + Math.abs(arr[n]-arr[n-2]);
+
+	    return dp[n] =  Math.min(once,twice);
+}
     public int minimumEnergy(int arr[],int N){
         int dp[] = new int [N+1];
         Arrays.fill(dp,-1);
-        return helper(arr,N,0,dp);
+        
+        
+        return helper(N-1,arr,dp);
     }
 }
