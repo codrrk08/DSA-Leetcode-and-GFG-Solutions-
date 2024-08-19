8307d9ced41a69b8ba1763064556fc3f152b8c0b
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-08-20 03:51:51 +0530

    Time: 17 ms (48.61%) | Memory: 46 MB (65.99%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,63 +1,46 @@
 class Solution {
-    static int[] nextSmallerIndex(int arr[]){
+    public int findArea(int arr[]){
         int n = arr.length;
-        int nsi[] = new int[n];
-        nsi[n-1] = n;
+        // int array[] = new int[n];
+        // for(int i = 0;i<n;i++){
+        //     array[i] = arr[i]-'0';
+        // }
         Stack<Integer> st = new Stack<>();
-        st.push(n-1);
-
-        for(int i=n-2; i>=0; i--){
-            while(st.size()!=0 && arr[st.peek()]>=arr[i]) st.pop();
-
-            if(st.size()==0) nsi[i] = n;
-            else nsi[i] = st.peek();
+        int maxArea = 0;
+        for(int i = 0;i<=n;i++){
+            while(!st.isEmpty() && (i==n || arr[st.peek()]>=arr[i])){
+                int height = arr[st.peek()];
+                st.pop();
+                int width;
+                if(st.isEmpty())
+                width = i;
+                else
+                width = i-st.peek()-1;
+                maxArea = Math.max(maxArea,(width*height));
+            }
             st.push(i);
         }
-        return nsi;
-    }
-
-    static int[] previousSmallerIndex(int arr[]){
-        int n = arr.length;
-        int psi[] = new int[n];
-        psi[0] = -1;
-        Stack<Integer> st = new Stack<>();
-        st.push(0);
-
-        for(int i=1; i<n; i++){
-            while(st.size()!=0 && arr[st.peek()]>=arr[i]) st.pop();
-
-            if(st.size()==0) psi[i] = -1;
-            else psi[i] = st.peek();
-            st.push(i);
-        }
-        return psi;
-    }
-
-    static int findArea(int arr[]){
-        int n = arr.length;
-        int nsi[] = nextSmallerIndex(arr);
-        int psi[] = previousSmallerIndex(arr);
-        int area = 0;
-        for(int i=0; i<n; i++){
-            int h = arr[i];
-            int b = Math.abs(nsi[i]-psi[i])-1;
-            area = Math.max(area,h*b);
-        }
-        return area;
+        return maxArea;
     }
     public int maximalRectangle(char[][] matrix) {
-    
-        int area = 0;
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int arr[] = new int[m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j]=='0') arr[j] = 0;
-                else arr[j] = arr[j]+1;
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int prefix[][] = new int[r][c];
+        for(int j = 0;j<c;j++){
+            int sum = 0;
+            for(int i=0;i<r;i++){
+                sum+=matrix[i][j]-'0';
+                if(matrix[i][j]-'0'==0)
+                sum = 0;
+                prefix[i][j] = sum;
             }
-            area = Math.max(area,findArea(arr));
         }
-        return area;
-    }   
+
+
+        int max = 0;
+        for(int i = 0;i<r;i++){
+            max = Math.max(max,findArea(prefix[i]));
+        }
+        return max;
+    }
 }
