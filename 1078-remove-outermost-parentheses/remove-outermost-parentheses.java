d643d3d1fe59c66a6733e73b97e6a72cb3b9c59c
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-08-20 19:47:01 +0530

    Time: 8 ms (34.20%) | Memory: 41.9 MB (79.37%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,21 +1,37 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-       StringBuilder sb = new StringBuilder(s);
-       StringBuilder ans = new StringBuilder();
-       int countOpen = 0;
-       for(int i = 0;i<s.length();i++){
-            char ch = sb.charAt(i);
-            if(ch=='('){
-                if(countOpen>0)
-                ans.append('(');
-                countOpen++;
+    //    StringBuilder sb = new StringBuilder(s);
+    //    StringBuilder ans = new StringBuilder();
+    //    int countOpen = 0;
+    //    for(int i = 0;i<s.length();i++){
+    //         char ch = sb.charAt(i);
+    //         if(ch=='('){
+    //             if(countOpen>0)
+    //             ans.append('(');
+    //             countOpen++;
+    //         }
+    //         else{
+    //             countOpen--;
+    //             if(countOpen>0)
+    //             ans.append(')');
+    //         }
+    //    }
+    //    return ans.toString();
+        Stack<Character> openBracket = new Stack<>();
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                if(openBracket.size()>0){
+                    sb.append(s.charAt(i));
+                }
+                openBracket.push(s.charAt(i));
+            }else{
+                openBracket.pop();
+                if(openBracket.size()>0){
+                    sb.append(s.charAt(i));
+                }
             }
-            else{
-                countOpen--;
-                if(countOpen>0)
-                ans.append(')');
-            }
-       }
-       return ans.toString();
+        }
+        return sb.toString();
     }
 }
