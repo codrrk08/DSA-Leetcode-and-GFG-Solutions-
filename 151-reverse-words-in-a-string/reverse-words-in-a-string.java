0d93a74cb382a91949486aaa6f910e60280fdaea
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-08-21 00:43:34 +0530

    Time: 6 ms (85.51%) | Memory: 43 MB (69.94%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,24 +1,34 @@
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();
-        s = s.replaceAll("\\s+"," ");
-        System.out.println(s);
-        // StringBuilder sb = new StringBuilder(s);
-        StringBuilder ans = new StringBuilder();
-        String temp = "";
-        for(int i = 0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch!=' '){
-                temp+=ch;
-            }
-            else{
-                ans.insert(0," ");
-                ans.insert(1,temp);
-                temp="";
-            }
+        // s = s.trim();
+        // s = s.replaceAll("\\s+"," ");
+        // System.out.println(s);
+        // // StringBuilder sb = new StringBuilder(s);
+        // StringBuilder ans = new StringBuilder();
+        // String temp = "";
+        // for(int i = 0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     if(ch!=' '){
+        //         temp+=ch;
+        //     }
+        //     else{
+        //         ans.insert(0," ");
+        //         ans.insert(1,temp);
+        //         temp="";
+        //     }
             
+        // }
+        // ans.insert(0,temp);
+        // return ans.toString();
+        String[] words = s.trim().split("\\s+");
+        StringBuilder ans = new StringBuilder();
+
+        for(int i = words.length - 1; i >= 0; i--){
+            ans.append(words[i]);
+            if(i != 0){
+                ans.append(" ");
+            }
         }
-        ans.insert(0,temp);
         return ans.toString();
     }
 }
