7810a8a3c3b3993e23a86ad2964a89c90e0e1482
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-08-23 16:03:37 +0530

    Time: 507 ms (24.72%) | Memory: 46.3 MB (7.75%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -3,17 +3,18 @@ class Solution {
         int sum = 0;
         // int arr[] = new int[26]; //array of alphabets
         int n = s.length();
-        for(int i = 0;i<n;i++){
-            int arr[] = new int[26]; 
+        int dp[][] = new int[n][n];
+        for(int i = n-1;i>=0;i--){
+            HashMap<Character,Integer> hm = new HashMap<>();
             for(int j = i;j<n;j++){
-                int ch = s.charAt(j);
-                arr[ch-'a']++;
+                char ch = s.charAt(j);
+                hm.put(ch,hm.getOrDefault(ch,0)+1);
                 int min = Integer.MAX_VALUE;
                 int max = 0;
-                for(int f:arr){
-                    if(f>0){
-                        min = Math.min(min,f);
-                        max = Math.max(max,f);
+                for(int val:hm.values()){
+                    if(val>0){
+                        min = Math.min(min,val);
+                        max = Math.max(max,val);
                     }
                 }
                 sum+=max-min;
