8ddd53fcbb633ffb0fa82461db205925cf3fe139
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-08-26 19:07:00 +0530

    Time: 39 ms (17.22%) | Memory: 45.3 MB (7.19%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -11,16 +11,35 @@ class Solution {
         // }
         // }
         // return count;
-        int chars[] = new int[3];
-        Arrays.fill(chars, -1);
+        // int chars[] = new int[3];
+        // Arrays.fill(chars, -1);
+        // int count = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        //     chars[s.charAt(i) - 'a'] = i;
+        //     if (chars[0] != -1 && chars[1] != -1 && chars[2] != -1){
+        //         count += 1 + Math.min(Math.min(chars[0], chars[1]), chars[2]);
+        //     }
+        // }
+        // return count;
+        HashMap<Character,Integer> hm = new HashMap<>();
         int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            chars[s.charAt(i) - 'a'] = i;
-            if (chars[0] != -1 && chars[1] != -1 && chars[2] != -1){
-                count += 1 + Math.min(Math.min(chars[0], chars[1]), chars[2]);
+        int len = s.length();
+        int i = 0;
+        int j = 0;
+        while(j<len){
+            char ch = s.charAt(j);
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+            while(hm.size()==3){
+                count+= len-j;
+                hm.put(s.charAt(i),hm.get(s.charAt(i))-1);
+                if(hm.get(s.charAt(i))==0)
+                hm.remove(s.charAt(i));
+                i++;
             }
+            j++;
         }
         return count;
+        
 
     }
 }
