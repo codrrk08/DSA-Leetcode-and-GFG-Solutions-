6e188be3ee22db1727a4208e5dd7933484596251
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-09-21 19:49:53 +0530

    Time: 20 ms (17.25%) | Memory: 48.9 MB (18.38%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -4,14 +4,19 @@ class Solution {
         for(int i = 1;i<=n;i++){
             ans.add(i);
         }
-        Collections.sort(ans, new Comparator<Integer>() {
-            // @Override
-            public int compare(Integer num1, Integer num2) {
-                // Convert the integers to strings for lexicographical comparison
-                String str1 = num1.toString();
-                String str2 = num2.toString();
-                return str1.compareTo(str2); // Lexicographical comparison
-            }
+        // Collections.sort(ans, new Comparator<Integer>() {
+        //     // @Override
+        //     public int compare(Integer num1, Integer num2) {
+        //         // Convert the integers to strings for lexicographical comparison
+        //         String str1 = num1.toString();
+        //         String str2 = num2.toString();
+        //         return str1.compareTo(str2); // Lexicographical comparison
+        //     }
+        // });
+        Collections.sort(ans,(a,b)->{
+            String s1 = Integer.toString(a);
+            String s2 = Integer.toString(b);
+            return s1.compareTo(s2);
         });
         return ans;
     }
