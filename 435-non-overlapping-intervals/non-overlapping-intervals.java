c24a8bf8118d38ade862d64fffd5850e2300e72d
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-10-15 01:17:10 +0530

    Time: 529 ms (5.02%) | Memory: 88.5 MB (12.02%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->{
+            return Integer.compare(a[1],b[1]);
+        });
+        int n = intervals.length;
+        for(int i = 0;i<n;i++){
+            System.out.println(intervals[i][0]+" "+intervals[i][1]);
+        }
+        int prev[] = intervals[0];
+        int count = 0;
+        for(int i = 1;i<n;i++){
+            if(intervals[i][0] < prev[1]){
+                count++;
+            }
+            else{
+                prev = intervals[i];
+            }
+        }
+        return count;
+        
+    }
+}
