50bb91b660dea38e3fafe181f096f68ce1eebed6
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-10-15 01:42:09 +0530

    Time: 47 ms (74.93%) | Memory: 73.1 MB (94.39%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -4,9 +4,9 @@ class Solution {
             return Integer.compare(a[1],b[1]);
         });
         int n = intervals.length;
-        for(int i = 0;i<n;i++){
-            System.out.println(intervals[i][0]+" "+intervals[i][1]);
-        }
+        // for(int i = 0;i<n;i++){
+        //     System.out.println(intervals[i][0]+" "+intervals[i][1]);
+        // }
         int prev[] = intervals[0];
         int count = 0;
         for(int i = 1;i<n;i++){
