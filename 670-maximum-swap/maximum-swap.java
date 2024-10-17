91a2846e71705349421a3850068598a357b57a12
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-10-17 16:34:51 +0530

    Time: 2 ms (8.36%) | Memory: 40.2 MB (79.95%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumSwap(int num) {
+        String s = Integer.toString(num);
+        char arr[] = s.toCharArray();
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(char ch:arr){
+            sb.append(ch);
+        }
+        sb.reverse();
+        if(sb.toString().equals(s)){
+            return num;
+        }
+        int index = -1;
+        int i = 0;
+        for(i = 0;i<sb.length();i++){
+            if(sb.charAt(i)==s.charAt(i)){
+                continue;
+            }
+            else{
+                index = s.lastIndexOf(sb.charAt(i));
+                break;
+            }
+        }
+        System.out.println(sb.toString());
+        StringBuilder sb2 = new StringBuilder(s);
+        char c1 = s.charAt(i);
+        char c2 = sb.charAt(i);
+        sb2.setCharAt(i,c2);
+        sb2.setCharAt(index,c1);
+        return Integer.parseInt(sb2.toString());
+        // return 0;
+    }
+}
