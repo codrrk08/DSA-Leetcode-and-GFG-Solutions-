8b43d1b823d4cfe6aa65de7dfc36e1a64f60bf41
Risshi Kudeshia <risshikudeshia@gmail.com>
2024-10-26 20:35:03 +0530

    Time: 2 ms (99.01%) | Memory: 41.6 MB (82.87%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,18 +1,27 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int x:arr){
-            hm.put(x,hm.getOrDefault(x,0)+1);
+        // HashMap to store <num,occurences>
+        Map<Integer, Integer> map = new HashMap<>();
+
+        
+        // if the key exists, increment its value; if not, start with 1.
+        for(int i = 0; i < arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        List<Integer> list = new ArrayList<>(hm.values());
-        HashMap<Integer,Integer> hm2 = new HashMap<>();
-        for(int x:list){
-            hm2.put(x,hm2.getOrDefault(x,0)+1);
+
+        // Use a set to store the frequenceies
+        Set<Integer> occurences = new HashSet<>();
+
+
+        for(int freq : map.values()){
+
+            // If the frequency is already in the set, return false
+            if(!occurences.add(freq)){
+                return false;
+            }
+
         }
-        for(Map.Entry<Integer,Integer> entry:hm2.entrySet()){
-            if(entry.getValue()>1)
-            return false;
-        }
+                
         return true;
     }
 }
